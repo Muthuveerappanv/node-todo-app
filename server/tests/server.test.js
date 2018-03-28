585237c19689393d59bc62d14a88654da04bba57
Muthu <muthu@muthus-MacBook-Pro.local>
2018-03-28 13:54:39 -0400

    modified server.js

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,12 @@ const request = require('supertest');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
+const todos = [{ text: 'test todos1' }, { text: 'test todos2' }];
+
 beforeEach((done) => {
-    Todo.remove().then(() => done());
+    Todo.remove().then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -20,7 +24,7 @@ describe('POST /todos', () => {
                 expect(res.body.text).toBe(text);
             }).end((err, res) => {
                 if (err) return err;
-                Todo.find().then((todos) => {
+                Todo.find({ text }).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text === text);
                     done();
@@ -31,15 +35,29 @@ describe('POST /todos', () => {
     it('Should test /todos for bad request', (done) => {
         request(app)
             .post('/todos')
-            .send({ })
+            .send({})
             .expect(400)
             .expect('Content-Type', /json/)
             .end((err, res) => {
                 if (err) return err;
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch(err => done(err));
             });
+    })
+});
+
+describe('GET /todos', () => {
+    it('Should test Get /todos response', (done) => {
+        var text = 'Test todo text';
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect(res => {
+                expect(res.body.todos[0].text).toEqual(todos[0].text);
+            })
+            .end(done);
     })
 });
