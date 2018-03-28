8375b811cc1072d685ef79332acc02d041c56c53
Muthu <muthu@muthus-MacBook-Pro.local>
2018-03-28 19:33:34 -0400

    GET by id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/User');
+const { ObjectID } = require('mongodb');
 
 const port = 3000 || process.env.PORT;
 
@@ -25,6 +26,18 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
     Todo.find().then(todos => {
         res.send({ todos })
+    }).catch(err => res.status(400).send(err));
+})
+
+app.get('/todos/:id', (req, res) => {
+    if (!ObjectID.isValid(req.params.id)) {
+        res.status(404).send();
+    }
+    Todo.findById(req.params.id).then(todo => {
+        if (!todo) {
+            res.status(404).send();
+        }
+        res.send({ todo })
     }).catch(err => res.status(400).send(err));
 })
 
