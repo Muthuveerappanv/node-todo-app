178ac8d0fc72d3a8021379993020eb3df9c76319
Muthu <muthu@muthus-MacBook-Pro.local>
2018-03-28 13:30:47 -0400

    modified server.js

--- server/server.js
+++ server/server.js
@@ -22,6 +22,12 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then(todos => {
+        res.send({ todos })
+    }).catch(err => res.status(400).send(err));
+})
+
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
 })
